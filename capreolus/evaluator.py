9eb5b4983d154375474e80303f6c735eec810ceb
Crystina <xzhangbx@ust.hk>
2020-05-17 02:41:23 -0400

    fall back to nromal metrics calc

--- capreolus/evaluator.py
+++ capreolus/evaluator.py
@@ -42,7 +42,7 @@ def mrr(qrels, runs, qids=None):
     rundoc_qrel = [(runs.get(q, {}), qrels.get(q, {})) for q in qids]
     with get_context("spawn").Pool(12) as p:
         ranks = p.starmap(_mrr, rundoc_qrel)
-    print("number of runs: ", len(ranks),  sum(ranks) / len(ranks), "number of zero: ", ranks.count(0.0))
+    # print("number of runs: ", len(ranks),  sum(ranks) / len(ranks), "number of zero: ", ranks.count(0.0))
 
     return sum(ranks) / len(ranks)
 
@@ -88,7 +88,6 @@ def calc_single_query_runs_trec(qrel, run, metrics):
 def _eval_runs(runs, qrels, metrics, dev_qids):
     assert isinstance(metrics, list)
 
-    # print(f"runs: {len(runs)}, qrels: {len(qrels)}, dev_qids: {len(dev_qids)}")
     calc_mrr = "mrr" in metrics
     if calc_mrr:
         metrics.remove("mrr")
@@ -100,17 +99,27 @@ def _eval_runs(runs, qrels, metrics, dev_qids):
     if metrics:  # in case only "mrr" is provided
         _verify_metric(metrics)
         qids = set(dev_qids) & set(qrels.keys()) & set(runs.keys())
-        qrel_run_metrics = [({q: qrels.get(q, {})}, {q: runs.get(q, {})}, metrics) for q in qids]
+        # qrel_run_metrics = [({q: qrels.get(q, {})}, {q: runs.get(q, {})}, metrics) for q in qids]
+        # qrel_run_metrics = [
+        #     (qrel, run, metrics) for qrel, run, metrics in qrel_run_metrics if len(qrel) > 0 and len(run) > 0]
+        # if not qrel_run_metrics:
+        #     logger.warning(f"empty qrels runs pair")
+        #     return 0
+        # print(f"qrel_run_metrics: {len(qrel_run_metrics)}")
+        # with get_context("spawn").Pool(12) as p:
+        #     trec_scores = p.starmap(calc_single_query_runs_trec, qrel_run_metrics)  # (Q, n_metrics)
+        #     assert len(trec_scores) == len(qrel_run_metrics)
+        # trec_scores = np.array(trec_scores).mean(axis=0).tolist()
+        # if len(trec_scores) != len(metrics):
+        #     raise ValueError(f"Mismatch trec_score and metrics, metrics: {metrics}; trec score: {trec_scores}")
+        # trec_scores = dict(zip(metrics, trec_scores))
+        # scores.update(trec_scores)
 
-        with get_context("spawn").Pool(12) as p:
-            trec_scores = p.starmap(calc_single_query_runs_trec, qrel_run_metrics)  # (Q, n_metrics)
-            assert len(trec_scores) == len(qrel_run_metrics)
+        dev_qrels = {qid: labels for qid, labels in qrels.items() if (qid in dev_qids and qid in runs)}
+        evaluator = pytrec_eval.RelevanceEvaluator(dev_qrels, _transform_metric(metrics))
 
+        trec_scores = [[metrics_dict.get(m, -1) for m in metrics] for metrics_dict in evaluator.evaluate(runs).values()]
         trec_scores = np.array(trec_scores).mean(axis=0).tolist()
-
-        print("trec_score / metrics: ", trec_scores, metrics)
-        if len(trec_scores) != len(metrics):
-            raise ValueError(f"Mismatch trec_score and metrics, metrics: {metrics}; trec score: {trec_scores}")
         trec_scores = dict(zip(metrics, trec_scores))
         scores.update(trec_scores)
 
@@ -175,24 +184,21 @@ def search_best_run(runfile_dir, benchmark, primary_metric, metrics=None, folds=
         if (f != "done" and not os.path.isdir(os.path.join(runfile_dir, f)))
     ]
 
-    # TMP
-    # p = "/home/xinyu1zhang/mpi-spring/capreolus/capreolus/csn_runfile_camel_2/filtered_bm25"
-    # p = "/home/xinyu1zhang/mpi-spring/capreolus/capreolus/csn_runfile_4/filtered_bm25"
-    # p = "/home/xinyu1zhang/mpi-spring/capreolus/capreolus/csn_runfile_lucenebm25/filtered_bm25/"
-    # p = "/home/xinyu1zhang/mpi-spring/capreolus/capreolus/csn_runfile_python_nocomma/filtered_bm25/"
-    # runfiles = [os.path.join(p, benchmark.cfg["lang"], "test.filtered.runfile")]
-
-    # runfiles = [f"/home/xinyu1zhang/.capreolus/cache/collection-codesearchnet_camelstemmer-True_lang-ruby/" \
-    #            f"index-anserini_indexstops-False_stemmer-porter/searcher-BM25_b-0.75_hits-1000_k1-1.2/" \
-    #            f"codesearchnet_corpus/searcher"]
-    # end of tmp
+    # runfiles = ["/home/xinyu1zhang/mpi-spring/capreolus/capreolus/csn_rm3/filtered_bm25/ruby/test.filtered.runfile"]
+    # print("runfiles: ", runfiles)
 
     best_scores = {s: {primary_metric: 0, "path": None} for s in folds}
     for runfile in tqdm(runfiles, desc="Processing available runfiles"):
         runs = Searcher.load_trec_run(runfile)
+        # print("loaded ", runfile)
+        if not runs:
+            logger.warning(f"empty file: {runfile}")
+            continue
+
         for s, v in folds.items():
+            dev_qids = set(v["train_qids"]) | set(v["predict"]["dev"])
             score = _eval_runs(
-                runs, benchmark.qrels, [primary_metric], dev_qids=(set(v["train_qids"]) | set(v["predict"]["dev"]))
+                runs, benchmark.qrels, [primary_metric], dev_qids=dev_qids
             )[primary_metric]
             if score > best_scores[s][primary_metric]:
                 best_scores[s] = {primary_metric: score, "path": runfile}
