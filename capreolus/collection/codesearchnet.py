fe34184eb67687b2b680a179f4ee1338b2e73e46
Crystina333 <xzhangbx@connect.ust.hk>
2020-07-06 12:01:11 +0800

    add docmap prep in collection

--- capreolus/collection/codesearchnet.py
+++ capreolus/collection/codesearchnet.py
@@ -1,7 +1,7 @@
 import json
 import pickle
 from zipfile import ZipFile
-from collections import OrderedDict
+from collections import defaultdict, OrderedDict
 
 from tqdm import tqdm
 from nirtools.text import preprocess
@@ -69,8 +69,10 @@ class CodeSearchNet(Collection):
         sents["final"] = sent
         return sents
 
-    def download_raw(self, lang, tmp_dir):
-        zip_file = "%s.zip" % lang
+    def download_raw(self):
+        tmp_dir = self.get_cache_path() / "tmp"
+        zip_file = "%s.zip" % self.config["lang"]
+
         zip_path = tmp_dir / zip_file
         lang_url = f"{self.url}/{zip_file}"
 
@@ -96,7 +98,7 @@ class CodeSearchNet(Collection):
             return document_dir
 
         document_dir.mkdir(exist_ok=True, parents=True)
-        raw_dir = self.download_raw(lang=self.config["lang"], tmp_dir=tmp_dir)
+        raw_dir = self.download_raw()
 
         pkl_path = raw_dir / (self.config["lang"] + "_dedupe_definitions_v2.pkl")
         self._docmap = self.parse_pkl(pkl_path, coll_filename)
@@ -114,9 +116,9 @@ class CodeSearchNet(Collection):
         with open(pkl_path, "rb") as f:
             codes = pickle.load(f)
 
-        id, docmap = 0, {}
+        docmap = defaultdict(dict)
         fout = open(trec_path, "w", encoding="utf-8")
-        for code in tqdm(codes, desc=f"Preparing the {lang} collection file"):
+        for id, code in tqdm(enumerate(codes), desc=f"Preparing the {lang} collection file"):
             docno = f"{lang}-FUNCTION-{id}"
             docs = self.process_text(
                 " ".join(code["function_tokens"]),
@@ -127,11 +129,16 @@ class CodeSearchNet(Collection):
             raw_doc, final_doc = docs["raw"], docs["final"]
             fout.write(document_to_trectxt(docno, final_doc))
 
-            if raw_doc in docmap:
-                logger.warning(f"duplicate code: current code snippet {code['url']} duplicate the document "
-                               f"{docmap[raw_doc]}}")
-            else:
-                docmap[raw_doc] = docno
-                id += 1
+            url = code["url"]
+            if raw_doc in docmap.get(url, {}):
+                logger.error(f"duplicate code: current code snippet {code['url']} duplicate the document "
+                               f"{docmap[url][raw_doc]}}")
+                # raise ValueError()
+            elif url in docmap:
+                docmap[url][raw_doc] = docno
+
+        # remove the code_tokens for the unique url-docid mapping
+        for url, docids in tqdm(docmap.items(), desc=f"Compressing the {lang} docid_map"):
+            docmap[url] = list(docids.values()) if len(docids) == 1 else docids  # {code_tokens: docid} -> [docid]
         fout.close()
         return docmap
