3cf53df39585ee70337b2b4771105dcffccfe5ee
Crystina <xzhangbx@ust.hk>
2020-07-06 23:44:01 -0400

    add get_qid, doc_id; (2) extract gzdir_generator to a class function (will be used by csn_distractor searcher)

--- capreolus/benchmark/codesearchnet.py
+++ capreolus/benchmark/codesearchnet.py
@@ -30,6 +30,19 @@ class CodeSearchNetCorpus(Benchmark):
     query_type = "title"
     file_fn = PACKAGE_PATH / "data" / "csn_corpus"
 
+    @property
+    def query_map(self):
+        if not hasattr(self, "_query_map"):
+            self.download_if_missing()
+
+        return self._query_map
+
+    def get_qid(self, query):
+        return self.query_map.get(query, -1)
+
+    def get_docid(self, url, doc):
+        return self.collection.get_docid(url, doc)
+
     def build(self):
         config = self.collection.config
         lang = config["lang"]
@@ -44,26 +57,40 @@ class CodeSearchNetCorpus(Benchmark):
 
         self.download_if_missing()
 
-    @property
-    def query_map(self):
-        if not hasattr(self, "_query_map"):
-            self.download_if_missing()
-
-        return self._query_map
+    def generate_parsed_doc_from_gz(self, dir):
+        """ generate parsed dict-format doc from all jsonl.gz files under given directory """
+        config = self.collection.config
+        for fn in sorted(dir.glob("*.jsonl.gz")):
+            f = gzip.open(fn, "rb")
+            for data in f:
+                data = json.loads(data)
+                codes = self.collection.process_text(
+                    sent=" ".join(data["code_tokens"]),
+                    lang=config["lang"],
+                    remove_keywords=config["removekeywords"],
+                    tokenize_code=config["tokenizecode"],
+                    remove_unichar=config["removeunichar"])
+                docstrings = self.collection.process_text(
+                    sent=" ".join(data["docstring_tokens"]),
+                    lang=config["lang"],
+                    remove_keywords=False,
+                    tokenize_code=config["tokenizecode"],
+                    remove_unichar=config["removeunichar"])
+                yield {
+                    "url": data["url"],
+                    "code_raw": codes["raw"],
+                    "code_final": codes["final"],
+                    "docstring_raw": docstrings["raw"],
+                    "docstring_final": docstrings["final"]
+                }
 
     def download_if_missing(self):
         files = [self.query_map_file, self.qrel_file, self.topic_file, self.fold_file]
         if all([f.exists() for f in files]):
+            self._query_map = json.load(open(self.query_map_file, "r"))
             return
 
-        def gen_doc_from_gzdir(dir):
-            """ generate parsed dict-format doc from all jsonl.gz files under given directory """
-            for fn in sorted(dir.glob("*.jsonl.gz")):
-                f = gzip.open(fn, "rb")
-                for doc in f:
-                    yield json.loads(doc)
-
-        lang = self.config["lang"]
+        lang = self.collection.config["lang"]
         raw_dir = self.collection.download_raw()
 
         # prepare folds, qrels, topics, docstring2qid  # TODO: shall we add negative samples?
@@ -75,35 +102,22 @@ class CodeSearchNetCorpus(Benchmark):
 
         for set_name in qids:
             set_path = raw_dir / lang / "final" / "jsonl" / set_name
-            for doc in gen_doc_from_gzdir(set_path):
-                code_dict = self.collection.process_text(
-                    sent=" ".join(doc["code_tokens"]),
-                    lang=lang,
-                    remove_keywords=self.config["removekeywords"],
-                    tokenize_code=self.config["tokenizecode"],
-                    remove_unichar=self.config["removeunichar"])
-                docstring_dict = self.collection.process_text(
-                    sent=" ".join(doc["docstring_tokens"]),
-                    lang=lang,
-                    remove_keywords=False,
-                    tokenize_code=self.config["tokenizecode"],
-                    remove_unichar=self.config["removeunichar"])
-
-                n_words_in_docstring = len(docstring_dict["final"].split())
+            for data in self.generate_parsed_doc_from_gz(set_path):
+                n_words_in_docstring = len(data["docstring_final"].split())
                 if n_words_in_docstring >= 1024:
                     logger.warning(
                         f"chunk query to first 1000 words otherwise TooManyClause would be triggered "
                         f"at lucene at search stage, ")
-                    docstring_dict["final"] = " ".join(docstring_dict["final"].split()[:1020])  # for TooManyClause Exception
+                    data["docstring_final"] = " ".join(data["docstring_final"].split()[:1020])  # for TooManyClause Exception
 
-                docid = self.collection.get_docid(doc["url"], code_dict["raw"])
-                qid = self._query_map.get(docstring_dict["raw"], str(len(self._query_map)))
+                docid = self.collection.get_docid(data["url"], data["code_raw"])
+                qid = self._query_map.get(data["code_raw"], str(len(self._query_map)))
                 qrel_file.write(f"{qid} Q0 {docid} 1\n")
 
-                if docstring_dict["raw"] not in self._query_map:
-                    self._query_map[docstring_dict["raw"]] = qid
+                if data["docstring_raw"] not in self._query_map:
+                    self._query_map[data["docstring_raw"]] = qid
                     qids[set_name].append(qid)
-                    topic_file.write(topic_to_trectxt(qid, docstring_dict["final"]))
+                    topic_file.write(topic_to_trectxt(qid, data["docstring_final"]))
 
         topic_file.close()
         qrel_file.close()
