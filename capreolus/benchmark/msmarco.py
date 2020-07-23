838bf990d79563999e1881dde865a728b0760c0f
Crystina333 <xzhangbx@connect.ust.hk>
2020-07-23 17:39:04 +0800

    support both shallow and deep qrels. force the judgement for both to be 16258 (the total number of judgement in deep qrel)

--- capreolus/benchmark/msmarco.py
+++ capreolus/benchmark/msmarco.py
@@ -5,11 +5,13 @@ import random
 
 from capreolus import constants, ConfigOption, Dependency, constants
 from capreolus.utils.common import download_file, remove_newline
-from capreolus.utils.trec import topic_to_trectxt
+from capreolus.utils.trec import topic_to_trectxt, load_qrels
+from capreolus.utils.loginit import get_logger
+
+logger = get_logger(__name__)
 
 PACKAGE_PATH = constants["PACKAGE_PATH"]
 from . import Benchmark
-
 
 # @Benchmark.register
 # class MSMarcoPassage(Benchmark):
@@ -27,10 +29,11 @@ class MSMarcoDoc(Benchmark):
     """ Currently only test fold query from TREC-DL 2019 is used """
     module_name = "msmarcodoc"
     dependencies = [Dependency(key="collection", module="collection", name="msmarco")]
+    config_spec = [
+        ConfigOption("judgementtype", "deep", "Whether to use deep or shallow qrels")
+    ]
 
-    qrel_file = PACKAGE_PATH / "data" / "qrels.msmarcodoc.txt"
-    topic_file = PACKAGE_PATH / "data" / "topics.msmarcodoc.txt"
-    fold_file = PACKAGE_PATH / "data" / "msmarcodoc.folds.json"
+    data_dir = PACKAGE_PATH / "data" / "msmarco"
     query_type = "title"
 
     @staticmethod
@@ -44,29 +47,70 @@ class MSMarcoDoc(Benchmark):
         return folds
 
     def build(self):
-        self.qrel_file.parent.mkdir(exist_ok=True)
+        data_dir = self.data_dir / self.config["judgementtype"]
+        self.qrel_file = data_dir / "qrels.msmarcodoc.txt"
+        self.topic_file = data_dir / "topics.msmarcodoc.txt"
+        self.fold_file = data_dir / "msmarcodoc.folds.json"
+
+        data_dir.mkdir(exist_ok=True, parents=True)
         self.download_if_missing()
+
+    def download_deep_qrel_topic(self, total_judgement_number):
+        qrel_url = "https://trec.nist.gov/data/deep/2019qrels-docs.txt"
+        if not self.qrel_file.exists():
+            download_file(qrel_url, self.qrel_file)
+        qrels = load_qrels(self.qrel_file)
+        qids = list(qrels.keys())
+        assert sum([len(qrels[qid]) for qid in qrels]) == total_judgement_number
+        return qids
+
+    def download_shallow_qrel_topic(self, total_judgement_number):
+        """ keep the first `total_judgement_number` queries from train set """
+        qrel_url = "https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-qrels.tsv.gz"
+        tmp_qrels_fn = self.get_cache_path() / "tmp" / "qrels.train.tsv"
+        if not tmp_qrels_fn.exists():
+            tmp_qrels_fn.parent.mkdir(exist_ok=True, parents=True)
+            download_file(qrel_url, tmp_qrels_fn)
+
+        qids = set()
+        with gzip.open(tmp_qrels_fn, "r") as fin, open(self.qrel_file, "w") as fout:
+            for line in fin:
+                if len(qids) == total_judgement_number:
+                    break
+
+                line = line.decode().strip()
+                qid = line.split()[0]
+                if qid in qids:  # only keep one judgement per query
+                    continue
+                fout.write(line)
+                qids.add(qid)
+
+        return list(qids)
+
+    def download_topic(self, qids, topic_url):
+        tmp_topic_fn = self.get_cache_path() / "tmp" / topic_url.split("/")[-1]  # "msmarco-test2019-queries.tsv.gz"
+        if not tmp_topic_fn.exists():
+            tmp_topic_fn.parent.mkdir(exist_ok=True, parents=True)
+            download_file(topic_url, tmp_topic_fn)
+        with gzip.open(tmp_topic_fn, "r") as fin, open(self.topic_file, "w") as fout:
+            for line in fin:
+                line = line.decode().strip()
+                qid, query = line.split("\t")
+                if qid in qids:
+                    fout.write(topic_to_trectxt(qid, query))
 
     def download_if_missing(self):
         if all([f.exists() for f in [self.qrel_file, self.topic_file, self.fold_file]]):
             return
 
-        qrel_url = "https://trec.nist.gov/data/deep/2019qrels-docs.txt"
-        if not self.qrel_file.exists():
-            download_file(qrel_url, self.qrel_file)
-
-        topic_url = "https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-test2019-queries.tsv.gz"
-        tmp_topic_fn = self.get_cache_path() / "tmp" / "msmarco-test2019-queries.tsv.gz"
-        if not tmp_topic_fn.exists():
-            tmp_topic_fn.parent.mkdir(exist_ok=True, parents=True)
-            download_file(topic_url, tmp_topic_fn)
-        qids = set()
-        with gzip.open(tmp_topic_fn, "r") as fin, open(self.topic_file, "w") as fout:
-            for line in fin:
-                line = line.decode().strip()
-                qid, query = line.split("\t")
-                fout.write(topic_to_trectxt(qid, query))
-                qids.add(qid)
+        total_judgement_number = 16258  # number of judgement contained in deep qrels, will keep the first 16258 judgement from shallow qrel for fair comparison
+        if self.config["judgementtype"] == "deep":
+            qids = self.download_deep_qrel_topic(total_judgement_number)
+            topic_url = "https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-test2019-queries.tsv.gz"
+        elif self.config["judgementtype"] == "shallow":
+            qids = self.download_shallow_qrel_topic(total_judgement_number)
+            topic_url = "https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-doctrain-queries.tsv.gz"
+        self.download_topic(qids, topic_url)
 
         fold_list = self.prepare_fold(list(qids))
         folds = {}
--- capreolus/collection/msmarco.py
+++ capreolus/collection/msmarco.py
@@ -1,9 +1,12 @@
 import gzip
 import shutil
+from time import time
 
 from capreolus.utils.common import download_file
 from capreolus.utils.loginit import get_logger
 from . import Collection
+
+logger = get_logger(__name__)
 
 
 @Collection.register
@@ -15,9 +18,10 @@ class MSMarco(Collection):
     # config_spec = [ConfigOption("path", "/GW/NeuralIR/nobackup/msmarco/trec_format", "path to corpus")]
 
     def download_if_missing(self):
-        coll_fn = self.get_cache_path() / "documents"
+        coll_dir = self.get_cache_path() / "documents"
+        coll_fn = coll_dir / "msmarco.collection.txt"
         if coll_fn.exists():
-            return coll_fn
+            return coll_dir
 
         coll_url = "https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs.trec.gz"
         tmp_coll_fn = self.get_cache_path() / "tmp" / "msmarco-docs.trec.gz"
@@ -25,8 +29,12 @@ class MSMarco(Collection):
             tmp_coll_fn.parent.mkdir(exist_ok=True, parents=True)
             download_file(coll_url, tmp_coll_fn)
 
-        coll_fn.parent.mkdir(exist_ok=True, parents=True)
-        with gzip.open(tmp_coll_fn, "r") as fin, open(coll_fn, "w", encoding="utf-8") as fout:
+        coll_dir.mkdir(exist_ok=True, parents=True)
+        logger.info(f"Extracting {tmp_coll_fn}...")
+        t = time()
+        with gzip.open(tmp_coll_fn, "rb") as fin, open(coll_fn, "wb") as fout:
             shutil.copyfileobj(fin, fout)
-
-        return coll_fn
+        duration = int(time() - t)
+        min, sec = duration // 60, duration % 60
+        logger.info(f"{tmp_coll_fn} extracted after {duration} seconds (00:{min}:{sec})")
+        return coll_dir
