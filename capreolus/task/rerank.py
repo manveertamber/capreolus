07a9ccebaef3183f68091cf266f77314d29ae5b3
Crystina333 <xzhangbx@connect.ust.hk>
2020-08-17 14:36:53 +0800

    refactor rerank task to use Run class with respect to sampler & evaluator
    able to run
    ```
    python run.py rerank.traineval with rank.searcher.name=BM25 rank.searcher.k1=0.9 rank.searcher.b=0.4 benchmark.name=robust04 reranker.name=KNRM reranker.trainer.niters=4
    ```
    from begininig to end
    Result (similar to the result before refactor):
    2020-08-17 02:35:48,169 - INFO - capreolus.task.rerank.evaluate - rerank: fold=s1 dev metrics: {'P_1': 0.48, 'P_10': 0.376, 'P_20': 0.327, 'P_5': 0.436, 'judged_10': 0.936, 'judged_20': 0.926, 'judged_200': 0.713, 'map': 0.207928258142067, 'mrr': 0.6040457647548392, 'ndcg_cut_10': 0.37987604014272647, 'ndcg_cut_20': 0.35910154010418943, 'ndcg_cut_5': 0.41792916100303423, 'recall_100': 0.35174567190814715, 'recall_1000': 0.6689428916858007, 'recip_rank': 0.5940457647548392}
    2020-08-17 02:35:50,377 - INFO - capreolus.task.rerank.evaluate - rerank: fold=s1 test metrics: {'P_1': 0.38, 'P_10': 0.396, 'P_20': 0.32299999999999995, 'P_5': 0.43999999999999995, 'judged_10': 0.954, 'judged_20': 0.9470000000000001, 'judged_200': 0.7349999999999999, 'map': 0.22551422281210384, 'mrr': 0.5561916007084854, 'ndcg_cut_10': 0.384193583320423, 'ndcg_cut_20': 0.3629195620685225, 'ndcg_cut_5': 0.3927447454254739, 'recall_100': 0.3994107945734405, 'recall_1000': 0.6882346581316194, 'recip_rank': 0.5661916007084854}
    2020-08-17 02:35:50,377 - INFO - capreolus.task.rerank.evaluate - rerank: skipping cross-validated metrics because results exist for only 1/5 folds

--- capreolus/task/rerank.py
+++ capreolus/task/rerank.py
@@ -9,6 +9,7 @@ from capreolus.sampler import TrainTripletSampler, PredSampler
 from capreolus.searcher import Searcher
 from capreolus.task import Task
 from capreolus.utils.loginit import get_logger
+from capreolus.utils.runobj import Runs
 
 logger = get_logger(__name__)
 
@@ -43,8 +44,7 @@ class RerankTask(Task):
         self.rank.search()
         rank_results = self.rank.evaluate()
         best_search_run_path = rank_results["path"][fold]
-        best_search_run = Searcher.load_trec_run(best_search_run_path)
-
+        best_search_run = Runs(best_search_run_path, buffer=self.benchmark.collection.is_large_collection)
         return self.rerank_run(best_search_run, self.get_results_path())
 
     def rerank_run(self, best_search_run, train_output_path, include_train=False):
@@ -56,32 +56,31 @@ class RerankTask(Task):
         dev_output_path = train_output_path / "pred" / "dev"
         logger.debug("results path: %s", train_output_path)
 
-        docids = set(docid for querydocs in best_search_run.values() for docid in querydocs)
+        # TODO: don't let each extractor put all id2tok in memory
         self.reranker.extractor.preprocess(
-            qids=best_search_run.keys(), docids=docids, topics=self.benchmark.topics[self.benchmark.query_type]
+            qids=best_search_run.keys(),
+            docids=best_search_run.docids(),
+            topics=self.benchmark.topics[self.benchmark.query_type]
         )
         self.reranker.build_model()
         self.reranker.searcher_scores = best_search_run
 
-        train_run = {qid: docs for qid, docs in best_search_run.items() if qid in self.benchmark.folds[fold]["train_qids"]}
-        # For each qid, select the top 100 (defined by config["threshold") docs to be used in validation
-        dev_run = defaultdict(dict)
-        # This is possible because best_search_run is an OrderedDict
-        for qid, docs in best_search_run.items():
-            if qid in self.benchmark.folds[fold]["predict"]["dev"]:
-                for idx, (docid, score) in enumerate(docs.items()):
-                    if idx >= threshold:
-                        break
-                    dev_run[qid][docid] = score
-
         # Depending on the sampler chosen, the dataset may generate triplets or pairs
         train_dataset = self.sampler
         train_dataset.prepare(
-            train_run, self.benchmark.qrels, self.reranker.extractor, relevance_level=self.benchmark.relevance_level,
+            best_search_run,
+            self.benchmark.folds[fold]["train_qids"],
+            self.benchmark.qrels,
+            self.reranker.extractor,
+            relevance_level=self.benchmark.relevance_level,
         )
         dev_dataset = PredSampler()
         dev_dataset.prepare(
-            dev_run, self.benchmark.qrels, self.reranker.extractor, relevance_level=self.benchmark.relevance_level,
+            best_search_run,
+            self.benchmark.folds[fold]["predict"]["dev"],
+            self.benchmark.qrels,
+            self.reranker.extractor,
+            relevance_level=self.benchmark.relevance_level,
         )
 
         self.reranker.trainer.train(
@@ -99,18 +98,13 @@ class RerankTask(Task):
         dev_output_path = train_output_path / "pred" / "dev" / "best"
         dev_preds = self.reranker.trainer.predict(self.reranker, dev_dataset, dev_output_path)
 
-        test_run = defaultdict(dict)
-        # This is possible because best_search_run is an OrderedDict
-        for qid, docs in best_search_run.items():
-            if qid in self.benchmark.folds[fold]["predict"]["test"]:
-                for idx, (docid, score) in enumerate(docs.items()):
-                    if idx >= threshold:
-                        break
-                    test_run[qid][docid] = score
-
         test_dataset = PredSampler()
         test_dataset.prepare(
-            test_run, self.benchmark.qrels, self.reranker.extractor, relevance_level=self.benchmark.relevance_level
+            best_search_run,
+            self.benchmark.folds[fold]["predict"]["test"],
+            self.benchmark.qrels,
+            self.reranker.extractor,
+            relevance_level=self.benchmark.relevance_level,
         )
         test_output_path = train_output_path / "pred" / "test" / "best"
         test_preds = self.reranker.trainer.predict(self.reranker, test_dataset, test_output_path)
@@ -118,10 +112,14 @@ class RerankTask(Task):
         preds = {"dev": dev_preds, "test": test_preds}
 
         if include_train:
-            train_dataset = PredSampler(
-                train_run, self.benchmark.qrels, self.reranker.extractor, relevance_level=self.benchmark.relevance_level,
+            train_dataset = PredSampler()
+            train_dataset.prepare(
+                best_search_run,
+                self.benchmark.folds[fold]["train_qids"],
+                self.benchmark.qrels,
+                self.reranker.extractor,
+                relevance_level=self.benchmark.relevance_level,
             )
-
             train_output_path = train_output_path / "pred" / "train" / "best"
             train_preds = self.reranker.trainer.predict(self.reranker, train_dataset, train_output_path)
             preds["train"] = train_preds
@@ -136,7 +134,7 @@ class RerankTask(Task):
         best_search_run_path = rank_results["path"][fold]
         best_search_run = Searcher.load_trec_run(best_search_run_path)
 
-        docids = set(docid for querydocs in best_search_run.values() for docid in querydocs)
+        docids = set(docid for querydocs in best_search_run.docids() for docid in querydocs)
         self.reranker.extractor.preprocess(
             qids=best_search_run.keys(), docids=docids, topics=self.benchmark.topics[self.benchmark.query_type]
         )
