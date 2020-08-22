dcaff225a8a24ee0c9096b96d4ab9c20fd92a6ef
Crystina <xzhangbx@ust.hk>
2020-08-22 04:59:59 +0000

    add msmarco psg

---
+++ capreolus/collection/msmarco.py
@@ -0,0 +1,99 @@
+import os
+import gzip
+import shutil
+import tarfile
+from time import time
+
+from capreolus.utils.common import download_file
+from capreolus.utils.loginit import get_logger
+from capreolus.utils.trec import document_to_trectxt
+from . import Collection
+
+logger = get_logger(__name__)
+
+
+class MSMarcoMixin:
+    @staticmethod
+    def download_and_extract(url, tmp_dir, expected_fns=None):
+        tmp_dir.mkdir(exist_ok=True, parents=True)
+        gz_name = url.split("/")[-1]
+        output_gz = tmp_dir / gz_name
+        if not output_gz.exists():
+            output_gz.parent.mkdir(exist_ok=True, parents=True)
+            download_file(url, output_gz)
+
+        logger.info(f"Extracting {output_gz}...")
+        t = time()
+        if str(output_gz).endswith("tar.gz"):
+            tmp_dir = tmp_dir / gz_name.replace(".tar.gz", "")
+            if not (tmp_dir.exists() and set(os.listdir(tmp_dir)) == expected_fns):
+                print(output_gz)
+                with tarfile.open(output_gz, "r:gz") as f:
+                    f.extractall(path=tmp_dir)
+        else:
+            outp_fn = tmp_dir / gz_name.replace(".gz", "")
+            if not outp_fn.exists():
+                with gzip.open(output_gz, "rb") as fin, open(outp_fn, "wb") as fout:
+                    shutil.copyfileobj(fin, fout)
+
+        duration = int(time() - t)
+        min, sec = duration // 60, duration % 60
+        logger.info(f"{output_gz} extracted after {duration} seconds (00:{min}:{sec})")
+        return tmp_dir
+
+
+@Collection.register
+class MSMarcoPsg(Collection, MSMarcoMixin):
+    module_name = "msmarcopsg"
+    collection_type = "TrecCollection"
+    generator_type = "DefaultLuceneDocumentGenerator"
+    is_large_collection = True
+
+    def download_raw(self):
+        url = "https://msmarco.blob.core.windows.net/msmarcoranking/collectionandqueries.tar.gz"
+        tmp_dir = self.get_cache_path() / "tmp"
+        expected_fns = {"collection.tsv", "qrels.dev.small.tsv", "qrels.train.tsv", "queries.train.tsv", "queries.dev.small.tsv", "queries.dev.tsv", "queries.eval.small.tsv", "queries.eval.tsv"}
+        gz_dir = self.download_and_extract(url, tmp_dir, expected_fns=expected_fns)
+        return gz_dir
+
+    def download_if_missing(self):
+        coll_dir = self.get_cache_path() / "documents"
+        coll_fn = coll_dir / "msmarco.psg.collection.txt"
+        if coll_fn.exists():
+            return coll_dir
+
+        # convert to trec file
+        coll_tsv_fn = self.download_raw() / "collection.tsv"
+        coll_fn.parent.mkdir(exist_ok=True, parents=True)
+        with open(coll_tsv_fn, "r") as fin, open(coll_fn, "w", encoding="utf-8") as fout:
+            for line in fin:
+                docid, doc = line.strip().split("\t")
+                doc = doc.replace("<", "").replace("/>", "")
+                fout.write(document_to_trectxt(docid, doc))
+
+        return coll_dir
+
+
+@Collection.register
+class MSMarcoDoc(Collection, MSMarcoMixin):
+    module_name = "msmarcodoc"
+    config_keys_not_in_path = ["path"]
+    collection_type = "TrecCollection"
+    generator_type = "DefaultLuceneDocumentGenerator"
+    is_large_collection = True
+
+    def download_raw(self):
+        url = "https://msmarco.blob.core.windows.net/msmarcoranking/msmarco-docs.trec.gz"
+        tmp_dir = self.get_cache_path() / "tmp"
+        gz_dir = self.download_and_extract(url, tmp_dir)
+        return gz_dir
+
+    def download_if_missing(self):
+        coll_dir = self.get_cache_path() / "documents"
+        coll_fn = coll_dir / "msmarco.doc.collection.txt"
+        if coll_fn.exists():
+            return coll_dir
+
+        coll_tmp_fn = self.download_raw() / "msmarco-docs.trec"
+        shutil.move(coll_tmp_fn, coll_fn)
+        return coll_dir
---
+++ capreolus/searcher/msmarco.py
@@ -0,0 +1,62 @@
+import os
+from collections import defaultdict
+
+
+from capreolus import ConfigOption, Dependency, constants
+from capreolus.utils.loginit import get_logger
+from capreolus.utils.trec import load_trec_topics
+
+from . import Searcher
+
+logger = get_logger(__name__)
+
+
+@Searcher.register
+class MsmarcoPsg(Searcher):
+    module_name = "msmarcopsg_searcher"
+    dependencies = [Dependency(key="benchmark", module="benchmark", name="msmarcopsg")]
+
+    @staticmethod
+    def convert_to_trec_runs(msmarco_top1k_fn, train):
+        runs = defaultdict(dict)
+        with open(msmarco_top1k_fn, "r", encoding="utf-8") as f:
+            for line in f:
+               if train:
+                    qid, pos_pid, neg_pid = line.strip().split("\t")
+                    runs[qid][pos_pid] = len(runs.get(pos_pid, []))
+                    runs[qid][neg_pid] = len(runs.get(neg_pid, []))
+               else:
+                    qid, pid, _, _ = line.strip().split("\t")
+                    runs[qid][pid] = len(runs.get(qid, []))
+        return runs
+
+    def _query_from_file(self, topicsfn, output_path, cfg):
+        """ only query results in dev and test set are saved """
+        donefn = os.path.join(output_path, "done")
+        if os.path.exists(donefn):
+            return output_path
+
+        qids = set(load_trec_topics(topicsfn)["title"].keys())
+
+        allruns = {}
+        urls = [
+            "https://msmarco.blob.core.windows.net/msmarcoranking/qidpidtriples.train.full.tsv.gz",
+            "https://msmarco.blob.core.windows.net/msmarcoranking/top1000.dev.tar.gz",
+            "https://msmarco.blob.core.windows.net/msmarcoranking/top1000.eval.tar.gz"]
+
+        tmp_dir = self.get_cache_path() / "tmp"
+        for url in urls:
+            extract_file_name = url.split("/")[-1].replace(".gz", "").replace(".tar", "")
+            extract_dir = self.benchmark.collection.download_and_extract(url, tmp_dir, expected_fns={extract_file_name})
+            runs = self.convert_to_trec_runs(extract_dir / extract_file_name, train=(".train." in url))
+            allruns.update(runs)
+
+        assert len(set(allruns.keys()) - qids) == 0
+
+        logger.info("writing runs to file...")
+        output_path.mkdir(exist_ok=True, parents=True)
+        self.write_trec_run(preds=allruns, outfn=(output_path / "searcher"))
+
+        with open(donefn, "wt") as donef:
+            print("done", file=donef)
+        return output_path
